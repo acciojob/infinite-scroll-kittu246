4993479ace5d079e0d25fd0123e027a5d94c24ff
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-22 15:57:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ let containerHeight = container.clientHeight;
 let scrollHeight = container.scrollHeight;
 let scrolltop = container.scrollTop;
 let newItem = document.createElement("li");
-newItem.innerText="item1";
+newItem.innerText="item2";
 
 container.addEventListener("scroll",() =>{
-    console.log(scrolltop);
-    console.log(scrollHeight-containerHeight);
-if(Math.floor(scrolltop) == (scrollHeight-containerHeight)){
+    
+	
+if(Math.floor(scrolltop) === (scrollHeight-containerHeight)){
 
-    container.append(newItem,newItem);
+    container.appendChild(newItem);
+	 container.appendChild(newItem);
 }
 });
