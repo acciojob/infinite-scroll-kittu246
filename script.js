3c073c6aa25109b52652ac73667e44a6da6752d1
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-22 15:56:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
+ const container = document.getElementById("infi-list");
+let containerHeight = container.clientHeight;
+let scrollHeight = container.scrollHeight;
+let scrolltop = container.scrollTop;
+let newItem = document.createElement("li");
+newItem.innerText="item1";
 
+container.addEventListener("scroll",() =>{
+    console.log(scrolltop);
+    console.log(scrollHeight-containerHeight);
+if(Math.floor(scrolltop) == (scrollHeight-containerHeight)){
+
+    container.append(newItem,newItem);
+}
+});
